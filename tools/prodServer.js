191b9b57dc1ac73c523a9221b98f343dd04e5cfa
John Kole <jkole@merrimackcapital.com>
2016-01-17 15:16:21 -0500

    Adds debugging logs

--- tools/prodServer.js
+++ tools/prodServer.js
@@ -5,9 +5,12 @@ var server = express();
 var port = process.env.PORT || 8080;
 var distDir = path.join(__dirname, '../dist');
 
+console.log('DEBUG: Setting up server with distDir=' + distDir);
+
 server.use(express.static(distDir));
 
 server.get('/*', function(req, res) {
+  console.log('DEBUG: In universal get handler...');
     res.sendFile(distDir);
 });
 
